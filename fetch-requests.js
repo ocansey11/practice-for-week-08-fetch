e6577af2c4286a962cd909f00946614974f5f69e
Kevin Ocansey <kevinocansey11@gmail.com>
2023-05-04 19:49:18 +0100

    completed assignment

--- fetch-requests.js
+++ fetch-requests.js
@@ -3,24 +3,46 @@ Make fetch requests in the browser for each of the following tasks.
 Paste your code for fetch requests here once you finish each task.
 */
 
+const { response } = require("express")
+
 /* =============== 1. Print the status code of the response =============== */
-
-// Your code here
-
-
+    // Your code here
+    function printStatusCode(url,option){
+        const res = fetch(url) 
+        res
+        .then((response) => {
+            console.log(response.status)
+            //also return the statusCode just in case
+            return response.status
+            } 
+        )
+    }
+    printStatusCode("/products")
 
 /* ====== 2. Print true if the status of the response was successful ====== */
+    // Your code here
+    function printTrue(url,option){
+        
+        const res = fetch(url) 
+        res
+        .then(console.log(true))
+        .catch(error => console.log( "Error :", error))
+    }
+    // We expect this endPoint to return true
+    printTrue("/products")
 
-// Your code here
+    // We would expect this endPoint to return an Error
+    printTrue("/produkkcts")
 
-
+    // OR
+    const res = fetch(url) 
+    res.then(response => console.log(response.ok))
 
 /* =================== 3. Print the Content-Type Header =================== */
-
-// Your code here
-
+    // Your code here
+    res.then(response => console.log(response.headers.get("Content-Type")))
 
 
 /* ============== 4. Print the body of the response as text =============== */
-
-// Your code here
+    // Your code here4
+    res.then(response => console.log(response.text()))
